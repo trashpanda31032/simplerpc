6fd49e9f74b16b5e991384291c519317e55fcf75
trashpanda31032 <156652080+trashpanda31032@users.noreply.github.com>
2024-07-21 07:18:28 +0545

    Update config.js

--- config.js
+++ config.js
@@ -6,16 +6,16 @@ function reloadPresence(client) {
       //more command https://discordjs-self-v13.netlify.app/#/docs/docs/main/class/RichPresence or https://github.com/aiko-chan-ai/discord.js-selfbot-v13/blob/main/Document/RichPresence.md
         .setApplicationId("1119170929747050506") //You can replace with your own bot application id
         .setType("PLAYING") //PLAYING, STREAMING, LISTENING
-        .setName("Dead") //name of your activity
-        .setDetails("miawww...") //detail activity
+        .setName("Miaww") //name of your activity
+        .setDetails("dying for her [afk]") //detail activity
         .setStartTimestamp(global.startTime)
-        .setAssetsLargeImage("mp:icons/1120755328267583559/a65ec7583c9986ea1faf8c3f02259916.webp")
+        .setAssetsLargeImage("https://i.pinimg.com/originals/9e/f3/b6/9ef3b6489649dfe3f5b967044aa35c80.jpg")
         .setAssetsLargeText("Miawww")
-        .setAssetsSmallImage("mp:avatars/390832676778803200/f72c077c48fb9b5c42a1d44330ec5f22.png")
-        .setAssetsSmallText("Zaxerion")
-        .addButton('Facebook', "https://www.facebook.com/zaxerionn")
-        .addButton('Paypal', "https://paypal.me/zaxerion")
+        .setAssetsSmallImage("https://imageio.forbes.com/specials-images/imageserve/64421a10faabcf3a4aeb4797/Vector-Blue-Verified-Badge/960x0.jpg?format=jpg&width=960")
+        .setAssetsSmallText("Astute-tra")
+        .addButton('server1', "https://discord.gg/CKBVFUK6")
+        .addButton('server2', "https://discord.gg/xJ4NuaAt")
     client.user.setActivity(activity.toJSON())
-    client.user.setStatus("idle")
+    client.user.setStatus("dnd")
 };
 module.exports = reloadPresence;
